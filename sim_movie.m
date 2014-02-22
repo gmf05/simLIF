5f7cef60035041d5d561b818647c2ea5af7317b8
Grant <gfiddyment@gmail.com>
2014-02-22 15:54:37 -0500

    Simplified color axis

--- sim_movie.m
+++ sim_movie.m
@@ -1,4 +1,9 @@
-function sim_movie(V)
+function sim_movie(V,Cax)
+
+if nargin<2
+%   Cax = [min(min(V)), max(max(V))];
+  Cax = [-75 -55]; % good for voltage
+end
 
 global dt
 global NT
@@ -6,25 +11,21 @@ global E_cell_dim
 global I_cell_dim
 
 N_E_cells = prod(E_cell_dim);
-N_I_cells = prod(I_cell_dim);
 time = (1:NT)*dt;
+t_delay = .001; % pause used in plotting [seconds]
 % define handles sp1, sp2 which will be subplots?
 % sp1 = [2 1 1];
 % sp2 = [2 1 2];
-t_delay = .001;
-
-% minV = -75; maxV = -55;
-minV = min(min(V)); maxV = max(max(V));
 
 for t = 1:NT
   subplot(2,1,1)
   imagesc(reshape(V(1:N_E_cells,t),E_cell_dim));
-  caxis([minV,maxV]);
+  caxis(Cax);
   title(num2str(time(t)));
 
   subplot(2,1,2)
   imagesc(reshape(V(N_E_cells+1:end,t),I_cell_dim));
-  caxis([minV,maxV]);
+  caxis(Cax);
 
   pause(t_delay);
 end
